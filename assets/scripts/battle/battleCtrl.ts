01acda27775e4d02cf48e2b19fa791830f4b1129
yejunrong <yejunrong@weile>
2021-01-21 15:02:24 +0800

    导入测试战斗资源,构建resources

--- assets/scripts/battle/battleCtrl.ts
+++ assets/scripts/battle/battleCtrl.ts
@@ -1,12 +1,12 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Vec3, Quat } from 'cc';
 const { ccclass, property } = _decorator;
+
+const MOVE_SPEED = 0.1;
+const ROTATION_SPEED = 120;
+
+let tmpSpeed = new Vec3();
+let tmpRotation = new Quat();
 
 @ccclass('BattleCtrl')
 export class BattleCtrl extends Component {
@@ -14,14 +14,25 @@ export class BattleCtrl extends Component {
     // dummy = '';
 
     /* use `property` decorator if your want the member to be serializable */
-    // @property
-    // serializableDummy = 0;
+    @property(Node)
+    private mainRole: Node | null = null
 
     start () {
         // Your initialization goes here.
+
+        // this.role.getRotation()
+
+        // let a = this.mainRole?.getChildByName("attack_01")?.getChildByName("Bip001 R Hand Socket")
+        // console.log(a)
+        // a.active = false;
+
     }
 
-    // update (deltaTime: number) {
-    //     // Your update function goes here.
-    // }
+    update (dt: number) {
+        // tmpSpeed.x += dt * MOVE_SPEED;
+        // this.mainRole.translate(tmpSpeed);
+
+        // Quat.fromEuler(tmpRotation, 0, dt * ROTATION_SPEED, 0);
+        // this.mainRole?.rotate(tmpRotation);
+    }
 }
