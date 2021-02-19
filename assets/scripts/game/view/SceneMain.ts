5fbe7e1642b72a4414a92bfbc1df608d4056e3ac
c328279128 <chenweijin@weile.com>
2021-02-19 16:01:26 +0800

    升级为3.0正式版

--- assets/scripts/game/view/SceneMain.ts
+++ assets/scripts/game/view/SceneMain.ts
@@ -5,7 +5,7 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, resources, instantiate } from 'cc';
 import { BaseScene } from './BaseScene';
 const { ccclass, property } = _decorator;
 
@@ -21,6 +21,14 @@ export class SceneMain extends BaseScene {
     start () {
         // Your initialization goes here.
         this.initUI();
+        // this.initCity();
+    }
+    initCity(){
+        
+        // resources.load('prefabs_ui/main_city', (err:any,res:any)=>{
+        //     let p = instantiate( res );
+        //     this.curScene?.addChild(p);
+        // } );
     }
 
     // update (deltaTime: number) {
