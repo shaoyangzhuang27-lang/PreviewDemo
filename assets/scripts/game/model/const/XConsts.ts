e62f918a513edd54073335a7b5ebf52fcc4f6172
guogang <guogang@weile.com>
2021-03-10 20:35:51 +0800

    modify：添加酒馆抽卡结算界面资源

--- assets/scripts/game/model/const/XConsts.ts
+++ assets/scripts/game/model/const/XConsts.ts
@@ -276,4 +276,6 @@ export class XConsts{
     public static PUB_RECOMMEND_LINEUP_UI_KNIGHTARMOR : string = "UI_KnightArmor";
     public static PUB_RECOMMEND_LINEUP_UI_CAMPANALYSE : string = "UI_CampAnalyse";
 
+    public static SUMMON_SETTLE_TITLE : string = "DATA_Achievement6";  //获得英雄 
+
 } 
