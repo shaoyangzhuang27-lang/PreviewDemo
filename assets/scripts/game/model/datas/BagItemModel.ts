03f9d5ee923d7c3a8fc5c2f37fc552ccb7d1cef1
huangzhiqing <huangzhiqing@weile.com>
2021-03-12 17:47:53 +0800

    背包模块、装备道具信息

--- assets/scripts/game/model/datas/BagItemModel.ts
+++ assets/scripts/game/model/datas/BagItemModel.ts
@@ -4,11 +4,16 @@ import { XConsts } from "../const/XConsts";
 import { BaseModel } from "./BaseModel";
 import { NotifyMgr } from '../../control/NotifyMgr';
 import { TableName, ValueMgr } from "../ValueMgr";
+import { ItemEquipType } from '../../view/menu/ItemEquipCell';
 
 export class BagItemModel extends BaseModel{
     private _bagItemList:Map<number,number> = new Map<number,number>(); //道具id 对应数量
     private _bagEquipList:Map<number, number> = new Map<number,number>(); //装备id 对应数量
-    // private _fragItemList:Map<number,Map<number,number>> = new Map<number,Map<number,number>>(); //背包索引
+
+    //汇总英雄身上所有道具 
+    private _allItemList:Array<[Msg.TObjectType, number,number]> = new Array<[Msg.TObjectType,number,number]>();        
+
+    private _notSellItemStingList:Map<Msg.TObjectType, Array<string>> = new Map<Msg.TObjectType, Array<string>>();
 
     public initBagItemList(msg:Msg.GetPlayerDataA)
     {
@@ -16,23 +21,285 @@ export class BagItemModel extends BaseModel{
         this._bagEquipList.clear();
         for(let key in msg.equipBagList){
             let value = msg.equipBagList[key];
-            this._bagEquipList.set(Number(key), Number(value));
+            this._bagEquipList.set(Number(key), Number(value));            
         }
         for(let key in msg.usableItemList){
             let value = msg.usableItemList[key];
             this._bagItemList.set(Number(key), Number(value));
+
+            let tempMap = new Map<number,number>();
+            tempMap.set(Number(key), Number(value));
+            this._allItemList.push([Msg.TObjectType.EObject_UsableItem, Number(key),Number(value)]);
         }
+        this._setNotSellItemStringMap()
     }
 
     //获取背包道具
     public getBagItemList()
     {
         return this._bagItemList;
-    }
+    }    
 
     //获取背包装备
     public getBagEquipList()
     {
         return this._bagEquipList;
+    }
+
+    /** 账号所有的道具
+     *  @param 返回Array<[Msg.TObjectType, number,number]>
+     *  @param Array[0] 表示类型
+     *  @param Array[1] 表示key
+     *  @param Array[2] 表示value
+     * */
+
+    public getAllGoods(){
+        let playerInfo = this._gameModel.getPlayerModel().getPlayerInfo()
+        //英雄升级点
+        if(playerInfo.heroUpgradeExp> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_UpgradePoint,0, Number(playerInfo.heroUpgradeExp)]);
+        }
+
+        //英雄进阶点
+        if(playerInfo.heroAdvanceExp> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_AdvanceExp, 0, Number(playerInfo.heroAdvanceExp)]);
+        }
+        //魔法尘
+        if(playerInfo.magicDust> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_MagicDust, 0, Number(playerInfo.magicDust)]);
+        }
+        //召唤卷轴
+        if(playerInfo.basicSummonScroll> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_BaseSummonScroll, 0, Number(playerInfo.basicSummonScroll)]);
+        }
+        if(playerInfo.heroicSummonScroll> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_HeroicSummonScroll, 0, Number(playerInfo.heroicSummonScroll)]);
+        }
+        //任务卷轴
+        if(playerInfo.basicHeroMissionScroll> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_BaseMissionScroll, 0, Number(playerInfo.basicHeroMissionScroll)]);
+        }
+        if(playerInfo.heroicHeroMissionScroll> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_HeroicMissionScroll, 0, Number(playerInfo.heroicHeroMissionScroll)]);
+        }
+        //灵魂石
+        if(playerInfo.soulStone> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_SoulStone, 0, Number(playerInfo.soulStone)]);
+        }
+        //奇迹宝石
+        if(playerInfo.miracleGem> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_MiracleGem, 0, Number(playerInfo.miracleGem)]);
+        }
+        //奇迹碎片
+        if(playerInfo.miracleShard> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_MiracleShard, 0, Number(playerInfo.miracleShard)]);
+        }
+        //PVP战书
+        // if(bagModel.getPVPTicket()> 0)
+        // {
+        //     this._allItemList.push([Msg.TObjectType.EObject_UpgradePoint, 0, Number(playerInfo.heroUpgradeExp)]);
+        //     this._initPrefab(res,0,bagModel.getPVPTicket(),Msg.TObjectType.EObject_PVPTicket);
+        // }
+        //筹码
+        if(playerInfo.normalChip> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_NormalChip, 0, Number(playerInfo.normalChip)]);
+        }
+        if(playerInfo.advancedChip> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_AdvancedChip, 0, Number(playerInfo.advancedChip)]);
+        }
+        //声望
+        if(playerInfo.reputation> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_Reputation, 0, Number(playerInfo.reputation)]);
+        }
+        //幸运硬币
+        if(playerInfo.luckyCoin> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_LuckyCoin, 0, Number(playerInfo.luckyCoin)]);
+        }
+        //试炼点
+        if(playerInfo.trailPoint> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_TrailPoint, 0, Number(playerInfo.trailPoint)]);
+        }
+        //公会币
+        if(playerInfo.guildGold> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_GuildCoin, 0, Number(playerInfo.guildGold)]);
+        }
+        //宠物资源
+        if(playerInfo.PetExp> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_PetExp, 0, Number(playerInfo.PetExp)]);
+        }
+        if(playerInfo.PetStone> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_PetStone, 0, Number(playerInfo.PetStone)]);
+        }
+        //心愿宝石
+        if(playerInfo.WonderGem> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_WonderGem, 0, Number(playerInfo.WonderGem)]);
+        }
+        //锻造石
+        if(playerInfo.forgeStone> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_ForgeStone, 0, Number(playerInfo.forgeStone)]);            
+        }
+        //符文水晶
+        if(playerInfo.CollegeMoney> 0)
+        {
+            this._allItemList.push([Msg.TObjectType.EObject_CollegeMoney, 0, Number(playerInfo.CollegeMoney)]);
+        }
+
+        return this._allItemList;
+    }
+
+    //战书数量
+    public getPVPTicket():number
+    {
+        return 0;
+    }
+
+    //返回指定道具/装备数量
+    public getItemCountByKey(key:number,itemType:number):number
+    {
+        let count:number = 0;
+        if(itemType == ItemEquipType.goods)
+        {
+            if(this._bagItemList.has(key))
+            {
+                count = Number(this._bagItemList.get(key));
+            }
+        }
+        else if(itemType == ItemEquipType.equip)
+        {
+            if(this._bagEquipList.has(key))
+            {
+                count = Number(this._bagEquipList.get(key));
+            }
+        }
+        
+        return count;
+    }
+
+    private _setNotSellItemStringMap()
+    {
+        this._notSellItemStingList.clear()
+
+        let templist:string[] = new Array<string>();
+        templist = ["UI_Name_Money","UI_Desc_Money"]    //金钱
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_Money,templist);
+
+        templist = ["UI_Name_Exp","UI_Desc_Exp"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_Exp,templist);
+
+        templist = ["UI_Name_UpgradePoint","UI_Desc_UpgradePoint"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_UpgradePoint,templist);
+
+        templist = ["UI_Name_Vrmb","UI_Desc_Vrmb"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_VRmb,templist);
+
+        templist = ["UI_Name_MagicDust","UI_Desc_MagicDust"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_MagicDust,templist);
+
+        templist = ["UI_Name_AdvanceExp","UI_Desc_AdvanceExp"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_AdvanceExp,templist);
+
+        templist = ["UI_Name_BaseSummonScroll","UI_Desc_BaseSummonScroll"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_BaseSummonScroll,templist);
+
+        templist = ["UI_Name_HeroicSummonScroll","UI_Desc_HeroicSummonScroll"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_HeroicSummonScroll,templist);
+
+        templist = ["UI_Name_BaseMissionScroll","UI_Desc_BaseMissionScroll"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_BaseMissionScroll,templist);
+
+        templist = ["UI_Name_HeroicMissionScroll","UI_Desc_HeroicMissionScroll"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_HeroicMissionScroll,templist);
+
+        templist = ["UI_Name_SoulStone","UI_Desc_SoulStone"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_SoulStone,templist);
+
+        templist = ["UI_Name_MiracleGem","UI_Desc_MiracleGem"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_MiracleGem,templist);
+
+        templist = ["UI_Name_MiracleShard","UI_Desc_MiracleShard"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_MiracleShard,templist);
+
+        templist = ["UI_Name_SkillMaterial","UI_Desc_SkillMaterial"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_SkillMaterial,templist);
+
+        templist = ["UI_Name_Pvpticket","UI_Desc_Pvpticket"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_PVPTicket,templist);
+
+        templist = ["UI_Name_NormalChip","UI_Desc_NormalChip"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_NormalChip,templist);
+
+        templist = ["UI_Name_AdvancedChip","UI_Desc_AdvancedChip"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_AdvancedChip,templist);
+
+        templist = ["UI_Name_LuckyCoin","UI_Desc_LuckyCoin"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_LuckyCoin,templist);
+
+        templist = ["UI_Name_Reputation","UI_Desc_Reputation"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_Reputation,templist);
+
+        templist = ["UI_Name_TrailPoint","UI_Desc_TrailPoint"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_TrailPoint,templist);
+
+        templist = ["UI_Name_TrailItem","UI_Desc_TrailItem"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_TrailItem,templist);
+
+        templist = ["UI_Name_GuildCoin","UI_Desc_GuildCoin"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_GuildCoin,templist);
+
+        templist = ["UI_Name_GuildExp","UI_Desc_GuildExp"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_GuildExp,templist);
+
+        templist = ["UI_Name_PetExp","UI_Desc_PetExp"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_PetExp,templist);
+
+        templist = ["UI_Name_PetStone","UI_Desc_PetStone"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_PetStone,templist);
+
+        templist = ["UI_Name_LoopQuestMoney","UI_Desc_LoopQuestMoney"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_LoopQuestMoney,templist);
+
+        templist = ["UI_Name_WonderGem","UI_Desc_WonderGem"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_WonderGem,templist);
+
+        templist = ["UI_Name_ForgeStone","UI_Desc_ForgeStone"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_ForgeStone,templist);
+
+        templist = ["UI_Name_CollegeMoney","UI_Desc_CollegeMoney"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_CollegeMoney,templist);
+
+        templist = ["UI_Name_LoopQuest2Money","UI_Desc_LoopQuest2Money"]
+        this._notSellItemStingList.set(Msg.TObjectType.EObject_LoopQuest2Money,templist);
+    }
+
+    public getItemDescByType(objType:Msg.TObjectType)
+    {
+        if(this._notSellItemStingList.has(objType))
+        {
+            let strArr = this._notSellItemStingList.get(objType) as Array<string>
+            return strArr
+        }
+        else{
+            return []
+        }
     }
 }
--- assets/scripts/game/view/menu/BagMain.ts
+++ assets/scripts/game/view/menu/BagMain.ts
@@ -1,7 +1,9 @@
 
 import { _decorator, Component, Node, ToggleContainer, EventHandler, Toggle, Vec3, tween, ScrollView, Game, resources, instantiate } from 'cc';
 import { GameModel } from '../../model/GameModel';
-import { ItemEquipCell } from './ItemEquipCell';
+import { ItemEquipType,ItemEquipCell } from './ItemEquipCell';
+import { PopItemUseWin } from '../pop/PopItemUseWin';
+import { PopMgr } from '../../control/PopMgr';
 const { ccclass, property } = _decorator;
 
 @ccclass('BagMain')
@@ -96,40 +98,55 @@ export class BagMain extends Component {
         let allEquipList = GameModel.getInstance().getBagModel().getBagEquipList();
         resources.load('prefabs_ui/main/itemEquipCell', (err:any,res:any)=>{
             for (let key of allEquipList.keys()) {
-                let value = allEquipList.get(key);  //数量
-                // let equipData = ValueMgr.getInstance().getItemByField(TableName.equip,Number(key)) as Config.equip.Record;
+                let value = allEquipList.get(key);  //数量   
                 let equipCell = instantiate(res) as Node;
                 this.scroll_EquipView.content?.addChild(equipCell);
-
-                let script = equipCell.getComponent("ItemEquipCell") as ItemEquipCell;
-                script.setItemType(Number(key),Number(value),2,(id:number,num:number)=>{
-                    this._itemEqipCallBack(id,num)
-                })
+                this._initPrefab(equipCell,Number(key),Number(value),ItemEquipType.equip); 
             }
         })   
     }
 
     private _initItemScrollview()
     {
-        let allItemList = GameModel.getInstance().getBagModel().getBagItemList();
+        let allGoodsList = GameModel.getInstance().getBagModel().getAllGoods();
+        let bagModel = GameModel.getInstance().getBagModel();
         resources.load('prefabs_ui/main/itemEquipCell', (err:any,res:any)=>{
-            for (let key of allItemList.keys()) {
-                let value = allItemList.get(key);  //数量
-                // let equipData = ValueMgr.getInstance().getItemByField(TableName.equip,Number(key)) as Config.equip.Record;
-                let equipCell = instantiate(res) as Node;
-                this.scroll_ItemView.content?.addChild(equipCell);
+            for (let index = 0; index < allGoodsList.length; index++) {
+                let itemGoods = allGoodsList[index];
 
-                let script = equipCell.getComponent("ItemEquipCell") as ItemEquipCell;
-                script.setItemType(Number(key),Number(value),1,(id:number,num:number)=>{
-                    this._itemEqipCallBack(id,num)
-                })
-            }
+                let itemCell = instantiate(res) as Node;
+                this.scroll_ItemView.content?.addChild(itemCell);
+
+                if(itemGoods[0] == Msg.TObjectType.EObject_UsableItem)
+                {
+                    this._initPrefab(itemCell,Number(itemGoods[1]),Number(itemGoods[2]),ItemEquipType.goods,Number(Msg.TObjectType.EObject_UsableItem));
+                }
+                else{
+                    this._initPrefab(itemCell,Number(itemGoods[0]),Number(itemGoods[2]),ItemEquipType.goods,Number(itemGoods[0]));
+                }                
+            }            
         })   
     }
 
-    private _itemEqipCallBack(itemID:number,itemType:number)
-    {
+    private _initPrefab(iconNode:Node,key:number,value:number,itemType:ItemEquipType, objType:number = 0)
+    {        
+        let script = iconNode.getComponent("ItemEquipCell") as ItemEquipCell;
+        script.setItemUseType(objType)
+        script.setItemType(Number(key),Number(value),itemType,(id:number,itemClickType:number,objClickType:number)=>{
+            this._itemEqipCallBack(id,itemClickType,objClickType)
+        })
+    }
 
+    private _itemEqipCallBack(itemID:number,itemType:number,objType:number = 0)
+    {
+        if(itemType == ItemEquipType.goods)
+        {
+            PopMgr.getInstance().popItemUseSellView(itemID,objType);
+        }
+        else{
+            PopMgr.getInstance().popEquipInfoView(itemID);            
+        }
+        
     }
 
     onDestroy()
